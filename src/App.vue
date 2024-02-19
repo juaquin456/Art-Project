c17e804cc4ef8077dd3a89024272d3b202b85d47
juaquin456 <juaquin.remon@utec.edu.pe>
2024-02-18 23:17:29 -0500

    home page structure done

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,85 @@
 <script setup lang="ts">
 import Map from './components/Map.vue'
+import {ref} from 'vue'
 
+const regionBackground = ref<string>('https://www.peru.travel/contenido/destino/imagen/es/9/1.2/principal/loreto.jpg')
+
+const regionsBackground: {[key: string]: string} = {
+  "Loreto": 'https://www.peru.travel/contenido/destino/imagen/es/9/1.2/principal/loreto.jpg',
+  "Ucayali": 'https://www.peru.travel/contenido/destino/imagen/es/9/1.2/principal/ucayali.jpg',
+  "San Martín": 'https://www.peru.travel/contenido/destino/imagen/es/9/1.2/principal/san-martin.jpg',
+  "Amazonas": 'https://ultimatejourneys.pe/wp-content/uploads/2016/03/Fotos-pintura-amazonica-1-1500x750.jpg',
+  "Madre de Dios": 'https://www.peru.travel/contenido/destino/imagen/es/9/1.2/principal/madre-de-dios.jpg',
+}
+
+function changeBackground(region: string) {
+  regionBackground.value = regionsBackground[region]
+}
 
 
 </script>
 
 <template>
-  <h1 class="title">ART PROJECT</h1>
-  <Map style="height: 100vh; width: 100vh;"/>
+ <div class="container">
+   <h1 class="title">AmazonArt</h1>
+   <p>¡Ven a conocer a artistas visuales indígenas de nuestra amazonía peruana!</p>
+  </div>
+  
+  <div class="demo-wrap">
+    <img
+      :src="regionBackground"
+      class="demo-bg"
+      alt="background"
+    >
+    <div class="demo-content">
+    <Map height="100vh" @current-region="changeBackground"/>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
+.container {
+  height: 100vh; 
+  display: flex; 
+  align-items: center; 
+  justify-content: center; 
+  flex-direction: column;
+}
+p {
+  text-align: center;
+  font-size: 1.5em;
+  color: #333;
+  margin-bottom: 50px;
+}
 .title {
-  display: absolute;
-  font-size: 3em;
+  font-size: 10em;
   text-align: center;
   margin-top: 0;
-  color: #646cff;
+  margin-bottom: 50px;
+  color: black;
   will-change: color;
   transition: color 300ms;
 }
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+
+.demo-bg {
+  opacity: 0.6;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: auto;
+  min-height: 100vh;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.demo-wrap {
+  overflow: hidden;
+  position: relative;
+  width: 100vw;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+
+.demo-content {
+  position: relative;
 }
+
 </style>
