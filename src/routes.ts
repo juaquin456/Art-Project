e2a250e09164e4bb09f965db029fa39351a52df6
LuisFern873 <luis.mendez.l@utec.edu.pe>
2024-02-21 20:25:30 -0500

    new page

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: "/", name: "Home", component: () => import("./views/Home.vue") },
+        { path: "/artistas/:departamento", name: "Arstists", component: () => import("./views/Artists.vue")}
     ],
 });
 
---
+++ src/views/Artists.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+
+
+</script>
+
+<template>
+    <div class="container">
+      <h1 class="title">AmazonArt</h1>
+      <p>Loreto</p>
+     </div>
+     
+     <div class="demo-wrap">
+       <div class="demo-content">
+       </div>
+     </div>
+   
+</template>
+
+<style scoped>
+
+p {
+  text-align: center;
+  font-size: 1.5em;
+  color: black;
+  margin-bottom: 50px;
+  font-family: "Sansita Swashed", system-ui;
+}
+
+</style>
