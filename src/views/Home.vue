2fb7c64dc8444faf191fb52b187878b81f12af11
juaquin456 <juaquin.remon@utec.edu.pe>
2024-02-22 11:03:32 -0500

    fix home scroll event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Map from '@/components/Map.vue'
-import {ref} from 'vue'
+import { onMounted, ref } from 'vue'
 
 import loretoUrl from '@/assets/loreto.jpg'
 import ucayaliUrl from '@/assets/ucayali.jpg'
@@ -9,8 +9,9 @@ import amazonasUrl from '@/assets/amazonas.jpg'
 import madreDeDiosUrl from '@/assets/madre_dios.jpg'
 
 const regionBackground = ref<string>(loretoUrl)
+let scrollValue: number;
 
-const regionsBackground: {[key: string]: string} = {
+const regionsBackground: { [key: string]: string } = {
   "Loreto": loretoUrl,
   "Ucayali": ucayaliUrl,
   "San Martín": sanMartinUrl,
@@ -23,36 +24,66 @@ function changeBackground(region: string) {
 }
 
 
+
+onMounted(() => {
+  console.log('mounted')
+  const bgElement = document.getElementById("bg");
+  const titleElement = document.getElementById("title-container");
+  const originalPosition = document.getElementById("title-container")!.getBoundingClientRect().top
+  scrollValue = originalPosition;
+  document.addEventListener("wheel", function(e: Event) {
+
+    const delta = (e as WheelEvent).deltaY
+
+    if (scrollValue >= window.innerHeight) {
+      if (delta < 0) {
+        scrollValue += delta
+        titleElement!.style.transform = `translate(-50%, -${scrollValue}px)`
+        bgElement!.style.filter = `blur(10px)`;
+      }
+    }
+    else if (scrollValue <= originalPosition) {
+      if (delta > 0) {
+        scrollValue += delta
+        titleElement!.style.transform = `translate(-50%, -${scrollValue}px)`
+      }
+    }
+    else {
+      scrollValue += delta
+      titleElement!.style.transform = `translate(-50%, -${scrollValue}px)`
+      if (scrollValue >= window.innerHeight) {
+        bgElement!.style.filter = `blur(0px)`;
+      }
+    }
+  })
+})
+
 </script>
 
 <template>
- <div class="container">
-   <h1 class="title">Amazonía</h1> <h1 class="title" style="font-size:7em;">al descubierto</h1>
-   <p>¡Ven a conocer a artistas visuales indígenas de nuestra amazonía peruana!</p>
-  </div>
-  
-  <div class="demo-wrap">
-    <img
-      :src="regionBackground"
-      class="demo-bg"
-      alt="background"
-    >
-    <div class="demo-content">
-    <Map height="100vh" @current-region="changeBackground"/>
+  <div id="container">
+    <div class="demo-wrap" id="bg">
+      <img :src="regionBackground" class="demo-bg" alt="background">
+      <div class="demo-content">
+        <Map height="100vh" @current-region="changeBackground" />
+      </div>
+    </div>
+    
+    <div id="title-container">
+      <h1 class="title">Amazonía</h1>
+      <h1 class="title" style="font-size:7em;">al descubierto</h1>
+      <p>Repositorio de artistas visuales indígenas de la amazonia peruana</p>
+
     </div>
   </div>
-
 </template>
 
 <style scoped>
-.container {
-  background: -webkit-linear-gradient(to right, #a7d5f2, #3E5902, #D2D904);
-  background: linear-gradient(to right, #a7d5f291, #3E59029d, #D2D9048c), url("@/assets/bg.jpg") center/cover no-repeat;
-  height: 100vh; 
-  display: flex; 
-  align-items: center; 
-  justify-content: center; 
-  flex-direction: column;
+#container {
+  position: relative;
+            width: 100vw;
+            height: 100vh;
+            overflow: hidden;
 }
 
 p {
@@ -64,6 +95,7 @@ p {
   background-color: rgb(175, 245, 118);
   border-radius: 10px;
 }
+
 .title {
   font-size: 13em;
   text-align: center;
@@ -71,7 +103,7 @@ p {
   margin-bottom: 20px;
   color: rgb(55, 161, 23);
   text-shadow: 2px 0 #fff, -2px 0 #fff, 0 2px #fff, 0 -2px #fff,
-               1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;
+    1px 1px #fff, -1px -1px #fff, 1px -1px #fff, -1px 1px #fff;
   will-change: color;
   transition: color 300ms;
   font-family: "Protest Riot", sans-serif;
@@ -88,14 +120,20 @@ p {
 }
 
 .demo-wrap {
-  overflow: hidden;
-  position: relative;
-  width: 100vw;
+  width: 100%;
+            height: 100%;
+            filter: blur(5px);
 }
 
+#title-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  transition: transform 0.5s ease-in-out;
+}
 
 .demo-content {
   position: relative;
 }
-
 </style>
