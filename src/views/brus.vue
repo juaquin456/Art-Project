e290572a12418aa50cc02051d888cd3e43c0fa85
LuisFern873 <luis.mendez.l@utec.edu.pe>
2024-02-25 15:42:26 -0500

    styling author page

--- src/views/brus.vue
+++ src/views/brus.vue
@@ -4,12 +4,17 @@
   </div>
   <div class="content">
     <p>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolor vitae voluptas facilis facere consequuntur, est tempore aperiam totam sequi quia sed quod sint ratione unde nulla dolorem saepe, tenetur eligendi quidem recusandae fuga laudantium ipsum quae? Totam fuga facere modi sit nulla quae dolores corporis aliquid ex? Et cupiditate vel provident totam pariatur voluptatem fugiat tempore consequuntur porro velit! Facere dolorum quis nihil tenetur, aperiam placeat asperiores vitae qui laborum enim nostrum sunt mollitia inventore numquam ipsum molestias tempora facilis nobis natus molestiae vero voluptatum, iure delectus nulla? Tenetur non vel earum modi, numquam laborum molestiae repudiandae adipisci commodi accusamus?
+      Este es un relato de vida profundamente enraizado en la rica herencia cultural de la Amazonía peruana. Desde su infancia en la comunidad de Pucaurquillo, este pintor autodidacta fue moldeado por las historias de sus padres y la sabiduría de los ancianos de su pueblo. Su encuentro con el antropólogo Jürg Gasché desencadenó un viaje de autoexploración y aprendizaje que lo llevó a descubrir su pasión por la pintura como medio de expresar la mitología, la historia y la cultura de su gente. A través de sus obras, busca tanto celebrar la alegría cósmica y la belleza de su entorno natural como abordar temas sociales y políticos que afectan a su comunidad y a la Amazonía en general.
+      <br> 
+      A medida que su arte lo llevó más allá de su pueblo natal, este pintor encontró nuevas perspectivas en las grandes ciudades del mundo, integrando sus experiencias urbanas en su obra y compartiendo su cultura ancestral con audiencias globales. Su viaje desde la selva amazónica hasta lugares como Lima, París, Washington, La Habana y Shanghai ha enriquecido su visión artística y lo ha llevado a reinterpretar y afirmar su identidad cultural como un componente vital de la diversidad cultural mundial.
     </p>
     <img class = "img" src="@/assets/brus.jpg" alt="">
   </div>
+  <div class="container">
+    <h2>Sus obras artísticas</h2>
+  </div>
   <div class="carousel">
-    <el-carousel :interval="4000" type="card" height="400px" style="width: 1000px;">
+    <el-carousel :interval="4000" type="card" height="400px" style="width: 1000px;" autoplay>
       <el-carousel-item>
         <img src="https://static.wixstatic.com/media/43ce63_5fdebce863fe42dd98c6ec6631d5547d~mv2.gif" alt="">
       </el-carousel-item>
@@ -17,14 +22,32 @@
         <img src="https://static.wixstatic.com/media/43ce63_91c5d1628bee4c678443923fc3c6e9df~mv2.gif" alt="">
       </el-carousel-item>
       <el-carousel-item>
-        <img src="https://static.wixstatic.com/media/43ce63_5fdebce863fe42dd98c6ec6631d5547d~mv2.gif" alt="">
+        <img src="https://static.wixstatic.com/media/43ce63_17dd90c1a4834ed9bef162d49689ccfb~mv2.gif" alt="">
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
-<style scoped>
 
+
+<style>
+
+body {
+  background: #654ea3;  /* fallback for old browsers */
+background: -webkit-linear-gradient(to left, #3E5902, #D2D904);  /* Chrome 10-25, Safari 5.1-6 */
+background: linear-gradient(to left, #3E5902, #D2D904); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+}
+h1 {
+  padding-top: 20px;
+  padding-bottom: 10px;
+}
+
+h2 {
+  font-size: 1.5em;
+  padding-top: 20px;
+  padding-bottom: 10px;
+}
 
 
 .content {
@@ -38,11 +61,12 @@
 p {
   width: 50%;
   padding: 30px;
+  text-align: justify;
 }
 
 .img {
-  background-color: black;
   width: 300px;
+  border-radius: 8px;
 }
 
 .carousel {
@@ -52,19 +76,19 @@ p {
   padding-bottom: 50px;
 }
 
-.el-carousel__item h3 {
-  color: #475669;
-  opacity: 0.75;
-  line-height: 200px;
-  margin: 0;
-  text-align: center;
+.el-carousel__item img {
+  height: 100%;
+  border-radius: 8px;
 }
 
 .el-carousel__item:nth-child(2n) {
-  background-color: #99a9bf;
+  /* background-color: #99a9bf; */
+  border-radius: 8px;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
-  background-color: #d3dce6;
+  /* background-color: #d3dce6; */
+  border-radius: 8px;
+  
 }
 </style>
